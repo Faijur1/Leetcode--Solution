be81ed7ff80b1d00725f4e797499202f5d9cf2f0
Md Faijur Rahaman <faijurrahaman232@gmail.com>
2026-09-02 22:12:28 +0530

    Time: 0 ms (100%), Space: 55.4 MB (78.13%) - LeetHub

---
+++ 3875-construct-uniform-parity-array-i/3875-construct-uniform-parity-array-i.js
@@ -0,0 +1,8 @@
+/**
+ * @param {number[]} nums1
+ * @return {boolean}
+ */
+var uniformArray = function(nums1) {
+    // It is always mathematically possible to achieve uniform parity.
+    return true;
+};
